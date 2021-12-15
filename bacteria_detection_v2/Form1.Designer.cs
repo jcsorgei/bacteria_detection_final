2be525ab876b20b0a909cbbd4a17d3253efb49c6
jcsorgei <csjozef88@gmail.com>
2021-12-15 15:47:52 +0100

    Add project files.

---
+++ bacteria_detection_v2/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace bacteria_detection_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.segmentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.watershedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fiileToolStripMenuItem = new System.Windows.Forms.ToolStripSeparator();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Watershed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(758, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Watersheded";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.segmentationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1406, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // segmentationToolStripMenuItem
+            // 
+            this.segmentationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.watershedToolStripMenuItem,
+            this.fiileToolStripMenuItem});
+            this.segmentationToolStripMenuItem.Name = "segmentationToolStripMenuItem";
+            this.segmentationToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.segmentationToolStripMenuItem.Text = "File";
+            // 
+            // watershedToolStripMenuItem
+            // 
+            this.watershedToolStripMenuItem.Name = "watershedToolStripMenuItem";
+            this.watershedToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.watershedToolStripMenuItem.Text = "Open";
+            this.watershedToolStripMenuItem.Click += new System.EventHandler(this.watershedToolStripMenuItem_Click);
+            // 
+            // fiileToolStripMenuItem
+            // 
+            this.fiileToolStripMenuItem.Name = "fiileToolStripMenuItem";
+            this.fiileToolStripMenuItem.Size = new System.Drawing.Size(100, 6);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(528, 531);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(546, 97);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(528, 531);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(711, 27);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(309, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(806, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Threshold (min):";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(720, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(995, 57);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 15);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "255";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(98, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 30);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Export Current Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1199, 81);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(74, 15);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Circle count:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(1080, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(321, 529);
+            this.listBox1.TabIndex = 18;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(546, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 19);
+            this.radioButton1.TabIndex = 19;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Threshold";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(546, 45);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(125, 19);
+            this.radioButton2.TabIndex = 20;
+            this.radioButton2.Text = "Threshold (Inverse)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(546, 70);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 19);
+            this.radioButton3.TabIndex = 21;
+            this.radioButton3.Text = "Otsu";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(239, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 30);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Export All Images";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(1038, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(335, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "128253 - Jozef Csörgei, 127989 - Frantisek Bukor,  - Gabriel Cseh";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(362, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 30);
+            this.button4.TabIndex = 26;
+            this.button4.Text = "Count bacteria";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1406, 633);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bacteria detection";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem segmentationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem watershedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator fiileToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ bacteria_detection_v2/Form1.cs
@@ -0,0 +1,222 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Jozef Csorgei - 128253, Frantisek Bukor - 127989, Gabriel Cseh - ??????
+
+namespace bacteria_detection_v2
+{
+    public partial class Form1 : Form
+    {
+        public int imgCount = 1;
+        public string fileName;
+        public double ThreshMin = 180;
+        public double ThreshMax = 255;
+        public int ThreshMode=0;
+        public Image <Gray, byte> _01Markers;
+        public Image<Gray, byte> _02Markers;
+        public Image<Gray, byte> _03Markers;
+        public Image<Gray, byte> markers;
+        public Image<Bgr, byte> circleimg;
+        public Image<Bgr, byte> img;
+        public Image<Bgr, byte> original;
+        public Image<Gray, byte> umat;
+        public Image<Gray, int> labels;
+        public CircleF[] circles;
+        public bool count;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = original.ToBitmap();
+            img = new Bitmap(pictureBox1.Image)
+                .ToImage<Bgr, byte>();
+            if (ThreshMode==0) {
+                markers = new Bitmap(pictureBox1.Image)
+                .ToImage<Gray, byte>()
+                .ThresholdBinary(new Gray(ThreshMin), new Gray(255));
+            }
+            else if (ThreshMode==1) {
+                markers = new Bitmap(pictureBox1.Image)
+                .ToImage<Gray, byte>()
+                .ThresholdBinaryInv(new Gray(ThreshMin), new Gray(255));
+            }
+            else  {
+                markers = new Bitmap(pictureBox1.Image).ToImage<Gray, byte>();
+                CvInvoke.Threshold(markers, markers, 0, 255,ThresholdType.Otsu);
+            }
+            Mat kernel1 = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Ellipse, new Size(21, 21), new Point(10, 10));// csinálunk egy elipszist aminek a mérete 21 és a közepe 10
+            CvInvoke.MorphologyEx(markers, markers, MorphOp.Erode, kernel1, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Reflect, new MCvScalar(1.0)); // egy erode transzformációt csinálunk a grayscale képen, hogy eltüntessük a kisebb imperfekciókat a képen
+            labels = new Image<Gray, Int32>(markers.Size);
+            markers.Data[10, 10,0] = 255; //berajzolunk egy újabb elemet a képre, hogy a watershed ezt az elemet nyújtsa ki a háttérnek
+            CvInvoke.ConnectedComponents(markers, labels);
+            CvInvoke.Watershed(img, labels);
+            Image<Gray, Byte> umat = labels.Convert<Gray, Byte>();
+            pictureBox1.Image = img.ToBitmap();
+            pictureBox2.Image = umat.ToBitmap();
+            count = false;
+        }
+
+        private void watershedImage(Bitmap img)
+        {
+            var srcImg = img
+                .ToImage<Bgr, byte>();
+            if (ThreshMode == 0)
+            {
+                markers = img
+                .ToImage<Gray, byte>()
+                .ThresholdBinary(new Gray(ThreshMin), new Gray(255));
+            }
+            else if (ThreshMode == 1)
+            {
+                markers = img
+                .ToImage<Gray, byte>()
+                .ThresholdBinaryInv(new Gray(ThreshMin), new Gray(255));
+            }
+            else
+            {
+                markers = img.ToImage<Gray, byte>();
+                CvInvoke.Threshold(markers, markers, 0, 255, ThresholdType.Otsu);
+            }
+
+            Directory.CreateDirectory($".\\{imgCount}");
+
+            _01Markers = markers;
+            _01Markers.Save($".\\{imgCount}\\01_markers.png");
+            Mat kernel1 = CvInvoke.GetStructuringElement(Emgu.CV.CvEnum.ElementShape.Ellipse, new Size(21, 21), new Point(10, 10));
+
+            CvInvoke.MorphologyEx(markers, markers, MorphOp.Erode, kernel1, new Point(-1, -1), 1, Emgu.CV.CvEnum.BorderType.Reflect, new MCvScalar(1.0)); 
+            _02Markers = markers;
+            _02Markers.Save($".\\{imgCount}\\02_markers.png");           
+            var labels = new Image<Gray, Int32>(markers.Size);
+            
+            markers.Data[10, 10, 0] = 255;
+            CvInvoke.ConnectedComponents(markers, labels);
+            CvInvoke.Watershed(srcImg, labels);
+
+            Image<Gray, Byte> umat = labels.Convert<Gray, Byte>();
+
+            _03Markers = umat;
+            _03Markers.Save($".\\{imgCount}\\03_markers.png");
+            pictureBox2.Image = umat.ToBitmap();
+            imgCount++;
+        }
+
+        private void watershedToolStripMenuItem_Click(object sender, EventArgs e) //Felső címsávból a kép megnyitása
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+                original = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+                fileName = openFileDialog1.FileName;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            ThreshMin = trackBar1.Value;
+            button1_Click(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _01Markers.Save("_01_mask_default.png");
+            _02Markers.Save("_02_markers_morphologyex.png");
+            _03Markers.Save("_03_segmented.png");
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                ThreshMode = 0;
+                radioButton2.Checked = false;
+                radioButton3.Checked = false;
+                button1_Click(sender, e);
+                trackBar1.Enabled = true;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                ThreshMode = 1;
+                radioButton1.Checked = false;
+                radioButton3.Checked = false;
+                button1_Click(sender, e);
+                trackBar1.Enabled = true;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                ThreshMode = 3;
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                button1_Click(sender, e);
+                trackBar1.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            imgCount = 1;
+            DirectoryInfo d = new DirectoryInfo(@"images");
+            FileInfo[] images = d.GetFiles("*.png");
+
+            foreach (var image in images)
+            {
+                var img = new Bitmap($".\\images\\{image.Name}");
+                watershedImage(img);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (count == false)
+            {
+
+                circleimg = img;
+                Image<Gray, Byte> umat = labels.Convert<Gray, Byte>();
+                Gray cannyThreshold = new Gray(50);
+                Gray cannyThresholdLinking = new Gray(30);
+                Gray circleAccumulatorThreshold = new Gray(50);
+                Image<Gray, Byte> cannyEdges = umat.Canny(cannyThreshold.Intensity, cannyThresholdLinking.Intensity);
+                circles = circleimg.Convert<Gray, Byte>().HoughCircles(
+                cannyThreshold,
+                circleAccumulatorThreshold,
+                0.3, //Resolution of the accumulator used to detect centers of the circles
+                cannyEdges.Height / 10, //min distance 
+                0, //min radius
+                300 //max radius
+                )[0]; //Get the circles from the first channel
+                listBox1.Items.Add($"{Path.GetFileName(fileName)} - Circle count: {circles.Count()}");
+                foreach (CircleF circle in circles)
+                {
+                    circleimg.Draw(circle, new Bgr(Color.Red), 4);
+                }
+                pictureBox1.Image = circleimg.ToBitmap();
+                count = true;
+            }
+            else { }
+        }
+    }
+}
